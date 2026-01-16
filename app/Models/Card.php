de87d1775d3f3aaa3fc78ab88e855ea2ce39d503
Valentyn Melnychenko <non.rp.1996@gmail.com>
2026-01-17 00:05:08 +0200

    Define relationships and fillable properties for Group and Card models

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Card extends Model
 {
-    //
+    protected $fillable = ['group_id', 'title', 'sort'];
+    public function group(): belongsTo
+    {
+        return $this->belongsTo(Group::class);
+    }
 }
--- app/Models/Group.php
+++ app/Models/Group.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Group extends Model
 {
-    //
+    protected $fillable = ['name', 'sort'];
+    public function cards(): HasMany
+    {
+        return $this->hasMany(Card::class)->orderBy('sort');
+    }
 }
