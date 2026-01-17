0fa155bff64b5b758be5a585ed3f99cd96fc5444
Valentyn Melnychenko <non.rp.1996@gmail.com>
2026-01-17 16:49:18 +0200

    Update dashboard route to use closure for rendering

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/settings.php';
